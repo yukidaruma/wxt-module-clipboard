1052e87b9aba361ecfc65675106ab963d939259e
yukidaruma <yukidaruma@users.noreply.github.com>
2025-10-26 09:10:03 +0900

    refactor: remove unnecessary externals config

--- build.config.ts
+++ build.config.ts
@@ -5,7 +5,6 @@ export default defineBuildConfig({
   rootDir: resolve(__dirname, "modules"),
   outDir: resolve(__dirname, "dist"),
   entries: ["index.ts", "client.ts", "background-plugin.ts"],
-  externals: ["wxt", "wxt/browser", "wxt/modules", "vite"],
   declaration: true,
   rollup: {
     emitCJS: true,
